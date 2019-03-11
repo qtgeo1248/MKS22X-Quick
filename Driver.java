777ea3f6b6e744b928ba532b718a9ad6a3daa1ce
qtgeo1248 <gzhou00@stuy.edu>
2019-03-11 17:25:45 -0400

    did case where the thing to the left is greater

--- Driver.java
+++ Driver.java
@@ -1,6 +1,12 @@
 public class Driver {
     public static boolean test(int[] data, int k) {
-        
+        for (int i = 0; i < data.length; i++) {
+            if (i < k) {
+                if (data[i] > data[k]) {
+                    return false;
+                }
+            }
+        }
     }
     public static void main(String[] args) {
         int[] test1 = {7, 3, 4, 10};
