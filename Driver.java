82942b973371ec89d061173d254cb6f916cd6beb
qtgeo1248 <gzhou00@stuy.edu>
2019-03-11 17:23:11 -0400

    created a test method to test my methods

---
+++ Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+    public static boolean test(int[] data, int k) {
+        
+    }
+    public static void main(String[] args) {
+        int[] test1 = {7, 3, 4, 10};
+        System.out.println(Quick.quickselect(test1, 2));
+    }
+}
