899dd5120d09a53a6987882fa7547c2efd6aff9c
qtgeo1248 <gzhou00@stuy.edu>
2019-03-11 17:27:58 -0400

    added comment and return true if everything's right

--- Driver.java
+++ Driver.java
@@ -2,15 +2,16 @@ public class Driver {
     public static boolean test(int[] data, int k) {
         for (int i = 0; i < data.length; i++) {
             if (i < k) {
-                if (data[i] > data[k]) {
+                if (data[i] > data[k]) { //when the thing in the left side is wrong
                     return false;
                 }
             } else if (i > k) {
-                if (data[i] < data[k]) {
+                if (data[i] < data[k]) { //when the thing in the right side is wrong
                     return false;
                 }
             }
         }
+        return true;
     }
     public static void main(String[] args) {
         int[] test1 = {7, 3, 4, 10};
