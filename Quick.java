d1a06c3689aeaeb8279ca461c14d8f2be689127b
qtgeo1248 <gzhou00@stuy.edu>
2019-03-11 14:41:18 -0400

    copy pasted comments now need to test

--- Quick.java
+++ Quick.java
@@ -14,43 +14,22 @@ public class Quick {
             int pivotIdx = start + Math.abs(gen.nextInt()) % (end - start + 1);
             int pivot = data[pivotIdx];
             data[pivotIdx] = data[0];
-            data[0] = pivot;
-            pivotIdx = 0;
+            data[0] = pivot; //switches the pivot and the first el
+            pivotIdx = 0; //records the value to be returned (will be changed as I go through algorithm)
             for (int i = 1; i < data.length; i++) {
                 int temp = data[i];
-                if (temp < pivot) {
+                if (temp < pivot) { //if you find something less, switch with the k's value and then add one to k
                     pivotIdx++;
-                    data[k] = temp;
+                    data[k] = temp; //switches the small with the right-most value
                 }
             }
             data[0] = data[pivotIdx];
-            data[pivotIdx] = pivot;
-            if (pivotIdx >= k) {
+            data[pivotIdx] = pivot; //puts the pivot in the right position
+            if (pivotIdx >= k) { //if you overshoot, you look at the left side
                 return quickH(data, k, start, pivotIdx);
-            } else {
+            } else { //if you undershoot, you look at the right side
                 return quickH(data, k, pivotIdx, end);
             }
-            return 0; //TEMP
         }
-    }
-
-    public static int partition (int[] data, int start, int end) {
-        Random gen = new Random();
-        int k = start + Math.abs(gen.nextInt()) % (end - start + 1);
-        int pivot = data[k];
-        data[k] = data[0];
-        data[0] = pivot; //switches the pivot and the first el
-        k = 0; //records the value to be returned (will be changed as I go through algorithm)
-        for (int i = 1; i < data.length; i++) {
-            int temp = data[i];
-            if (temp < pivot) { //if you find something less, switch with the k's value and then add one to k
-                k++; //adds one to the thing you return
-                data[i] = data[k];
-                data[k] = temp; //switches the small with the right-most value
-            }
-        }
-        data[0] = data[k];
-        data[k] = pivot; //puts the pivot in the right position
-        return k;
     }
 }
