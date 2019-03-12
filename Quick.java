b5a7b1ca2f372679469e73835bd616f041680b2d
qtgeo1248 <gzhou00@stuy.edu>
2019-03-12 11:31:17 -0400

    used K's method to do partition

--- Quick.java
+++ Quick.java
@@ -28,19 +28,17 @@ public class Quick {
         }
     }
 
-    public static int partition (int[] data, int start, int end) {
-        Random gen = new Random();
-        int k = start + Math.abs(gen.nextInt()) % (end - start + 1);
-        int pivot = data[k];
-        data[k] = data[0];
-        data[0] = pivot; //switches the pivot and the first el
-        k = 0; //records the value to be returned (will be changed as I go through algorithm)
-        for (int i = 1; i < data.length; i++) {
+    public static int partition(int[] data, int start, int end) {
+        int pivot = data[0];
+        int i = 0; //records the value to be returned (will be changed as I go through algorithm)
+        int j = data.length - 1; //records the index of the greater than
+        while (i != j) {
             int temp = data[i];
-            if (temp < pivot) { //if you find something less, switch with the k's value and then add one to k
-                k++; //adds one to the thing you return
-                data[i] = data[k];
-                data[k] = temp; //switches the small with the right-most value
+            if (temp < pivot) { //if you find something less, do nothing
+                i++; //adds one to the thing you return
+            } else { //if you find something more, move it to the end
+                data[i] = data[j];
+                data[j] = temp;
             }
         }
         data[0] = data[k];
