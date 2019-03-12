ac681bdf6f974cb1eb1acbe12e1dc5e86a5f6fbe
qtgeo1248 <gzhou00@stuy.edu>
2019-03-11 20:02:34 -0400

    finished my test function, not need to test it manually

--- Driver.java
+++ Driver.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Random;
 import java.lang.Math;
+import java.util.Arrays;
 
 public class Driver {
     public static boolean test(int[] data, int k, int ans) {
@@ -10,23 +11,29 @@ public class Driver {
         for (int i = 0; i < data.length; i++) {
             if (data[i] < ans) {
                 less++;
-            } else if (data == ans) {
+            } else if (data[i] == ans) {
                 same++;
             } else {
                 greater++;
             }
         }
+        return (less <= k) && (same + less >= k);
     }
     public static void main(String[] args) {
+        /*
         Random gen = new Random();
         ArrayList<Integer> wrong = new ArrayList<Integer>(); //this is the list of things you got wrong
-        for (int i = 0; i < 1000; i++) { //i is the size
+        for (int i = 1; i < 10; i++) { //i is the size
             int[] test = new int[i];
             for (int j = 0; j < test.length; j++) { //fills the array
-                test[j] = gen.nextInt();
+                test[j] = gen.nextInt() % 100;
             }
             int k = Math.abs(gen.nextInt()) % i;
             int ans = Quick.quickselect(test, k);
+            System.out.println(Arrays.toString(test));
+            System.out.println(k + ", " + ans);
+            System.out.println(test(test, k, ans));
         }
+        */
     }
 }
