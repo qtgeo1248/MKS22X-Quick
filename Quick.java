9d875f255708c4f4e687bdc3103f66a8362a36c2
qtgeo1248 <gzhou00@stuy.edu>
2019-03-11 21:20:17 -0400

    things i used to print

--- Quick.java
+++ Quick.java
@@ -1,4 +1,4 @@
-import java.util.RArrays;
+import java.util.Arrays;
 
 public class Quick {
 
@@ -7,27 +7,29 @@ public class Quick {
     }
 
     public static int quickH(int[] data, int k, int start, int end) { //TEMP MAKE PRIVATE LATER
-        if (start == k || end == k) { //base case when you have narrowed down everything
+        /*if (start == k || end == k) { //base case when you have narrowed down everything
             return data[k];
-        } else {
-            int pivot = data[start];
-            int pivotIdx = start; //records the value to be returned (will be changed as I go through algorithm)
-            for (int i = start + 1; i <= end; i++) {
-                int temp = data[i];
-                if (temp < pivot) { //if you find something less, switch with the k's value and then add one to k
-                    pivotIdx++;
-                    data[pivotIdx] = temp; //switches the small with the right-most value
-                }
-            }
-            data[start] = data[pivotIdx];
-            data[pivotIdx] = pivot; //puts the pivot in the right position
-            if (pivotIdx > k) { //if you overshoot, you look at the left side
-                return quickH(data, k, start, pivotIdx);
-            } else if (pivotIdx < k) { //if you undershoot, you look at the right side
-                return quickH(data, k, pivotIdx, end);
-            } else {
-                return pivot;
+        } else {*/
+        int pivot = data[start];
+        int pivotIdx = start; //records the value to be returned (will be changed as I go through algorithm)
+        System.out.println(Arrays.toString(data) + ", " + pivotIdx + ", " + pivot);
+        for (int i = start + 1; i <= end; i++) {
+            int temp = data[i];
+            if (temp < pivot) { //if you find something less, switch with the k's value and then add one to k
+                pivotIdx++;
+                data[i] = data[pivotIdx];
+                data[pivotIdx] = temp; //switches the small with the right-most value
             }
         }
+        data[start] = data[pivotIdx];
+        data[pivotIdx] = pivot; //puts the pivot in the right position
+        if (pivotIdx > k) { //if you overshoot, you look at the left side
+            return quickH(data, k, start, pivotIdx);
+        } else if (pivotIdx < k) { //if you undershoot, you look at the right side
+            return quickH(data, k, pivotIdx, end);
+        } else {
+            return pivot;
+        }
+        //}
     }
 }
