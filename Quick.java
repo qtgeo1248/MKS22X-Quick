9e07a356f898cfc7a7bb8b3ef1a15a9a0c336728
qtgeo1248 <gzhou00@stuy.edu>
2019-03-12 11:27:57 -0400

    apparantely I actually need my partition method from before so haha

--- Quick.java
+++ Quick.java
@@ -6,10 +6,7 @@ public class Quick {
         return quickH(data, k, 0, data.length - 1);
     }
 
-    public static int quickH(int[] data, int k, int start, int end) { //TEMP MAKE PRIVATE LATER
-        /*if (start == k || end == k) { //base case when you have narrowed down everything
-            return data[k];
-        } else {*/
+    public static int quickH(int[] data, int k, int start, int end) {
         int pivot = data[start];
         int pivotIdx = start; //records the value to be returned (will be changed as I go through algorithm)
         for (int i = start + 1; i <= end; i++) {
@@ -29,6 +26,25 @@ public class Quick {
         } else {
             return pivot;
         }
-        //}
+    }
+
+    public static int partition (int[] data, int start, int end) {
+        Random gen = new Random();
+        int k = start + Math.abs(gen.nextInt()) % (end - start + 1);
+        int pivot = data[k];
+        data[k] = data[0];
+        data[0] = pivot; //switches the pivot and the first el
+        k = 0; //records the value to be returned (will be changed as I go through algorithm)
+        for (int i = 1; i < data.length; i++) {
+            int temp = data[i];
+            if (temp < pivot) { //if you find something less, switch with the k's value and then add one to k
+                k++; //adds one to the thing you return
+                data[i] = data[k];
+                data[k] = temp; //switches the small with the right-most value
+            }
+        }
+        data[0] = data[k];
+        data[k] = pivot; //puts the pivot in the right position
+        return k;
     }
 }
